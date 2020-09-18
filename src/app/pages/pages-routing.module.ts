c84285135fadab6b24b24350efaed14d93a16661
ALEJANDRO CORDOBA <cordoba.alejandro@gmail.com>
2020-09-18 17:01:18 -0300

    Formulario Completo

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,70 +3,72 @@ import { Routes, RouterModule } from "@angular/router";
 import { LayoutComponent } from "./_layout/layout.component";
 
 const routes: Routes = [
-  {
-    path: "",
-    component: LayoutComponent,
-    children: [
-      {
-        path: "dashboard",
-        loadChildren: () =>
-          import("./dashboard/dashboard.module").then((m) => m.DashboardModule),
-          
-      },
-      {
-        path: "builder",
-        loadChildren: () =>
-          import("./builder/builder.module").then((m) => m.BuilderModule),
-      },
-      {
-        path: "registro",
-        loadChildren: () =>
-          import("../pages/registros/registro/registro.module").then((m) => m.RegistroModule
-          ),
-      },
-      {
-        path: "registros",
-        loadChildren: () =>
-          import("../pages/registros/registros.module").then((m) => m.RegistrosModule
-          ),
-      },
-      {
-        path: "user-management",
-        loadChildren: () =>
-          import("../modules/user-management/user-management.module").then(
-            (m) => m.UserManagementModule
-          ),
-      },
-      {
-        path: "ngbootstrap",
-        loadChildren: () =>
-          import("../modules/ngbootstrap/ngbootstrap.module").then(
-            (m) => m.NgbootstrapModule
-          ),
-      },
-      {
-        path: "material",
-        loadChildren: () =>
-          import("../modules/material/material.module").then(
-            (m) => m.MaterialModule
-          ),
-      },
-      {
-        path: "",
-        redirectTo: "dashboard",
-        pathMatch: "full",
-      },
-      {
-        path: "**",
-        redirectTo: "errors/404",
-        pathMatch: "full",
-      },
-    ],
-  },
+	{
+		path: "",
+		component: LayoutComponent,
+		children: [
+			{
+				path: "dashboard",
+				loadChildren: () =>
+					import("./dashboard/dashboard.module").then((m) => m.DashboardModule),
+			},
+			{
+				path: "builder",
+				loadChildren: () =>
+					import("./builder/builder.module").then((m) => m.BuilderModule),
+			},
+			{
+				path: "registro",
+				loadChildren: () =>
+					import("../pages/registros/registro/registro.module").then(
+						(m) => m.RegistroModule
+					),
+			},
+			{
+				path: "registros",
+				loadChildren: () =>
+					import("../pages/registros/registros.module").then(
+						(m) => m.RegistrosModule
+					),
+			},
+
+			{
+				path: "user-management",
+				loadChildren: () =>
+					import("../modules/user-management/user-management.module").then(
+						(m) => m.UserManagementModule
+					),
+			},
+			{
+				path: "ngbootstrap",
+				loadChildren: () =>
+					import("../modules/ngbootstrap/ngbootstrap.module").then(
+						(m) => m.NgbootstrapModule
+					),
+			},
+			{
+				path: "material",
+				loadChildren: () =>
+					import("../modules/material/material.module").then(
+						(m) => m.MaterialModule
+					),
+			},
+			{
+				path: "",
+				redirectTo: "dashboard",
+				pathMatch: "full",
+			},
+			{
+				path: "**",
+				redirectTo: "errors/404",
+				pathMatch: "full",
+			},
+		],
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule],
 })
 export class PagesRoutingModule {}
