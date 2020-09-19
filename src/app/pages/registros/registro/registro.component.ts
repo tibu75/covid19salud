17b1ffb6ec86772fdd195104198e2de75535bf61
LUIS ALBERTO PARADA <luisparadawagner@gmail.com>
2020-09-19 00:53:40 -0300

    Se agrega formulario 0800

--- src/app/pages/registros/registro/registro.component.ts
+++ src/app/pages/registros/registro/registro.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef } from "@angular/core";
-import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
-import { RenaperService } from "../../../services/ws/renaper.service";
-import { Subscription, Observable } from "rxjs";
-import { DomSanitizer } from "@angular/platform-browser";
-import { Router, ActivatedRoute } from "@angular/router";
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { FormArray, FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { RenaperService } from '../../../services/ws/renaper.service';
+import { Subscription, Observable } from 'rxjs';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-	selector: "app-registro",
-	templateUrl: "./registro.component.html",
-	styleUrls: ["./registro.component.scss"],
+	selector: 'app-registro',
+	templateUrl: './registro.component.html',
+	styleUrls: [ './registro.component.scss' ]
 })
 export class RegistroComponent implements OnInit {
 	closeResult: string;
-	personaForm: FormGroup;
-	form_registro: FormGroup;
-	renaper: any = {};
+	personaForm: FormGroup;	
 	data: any = {};
 	public cargando_datos: boolean = true;
 	public buscar_persona: boolean = true;
@@ -26,175 +24,74 @@ export class RegistroComponent implements OnInit {
 	constructor(
 		public _renaperService: RenaperService,
 		private router: Router,
-		private activadeRoute: ActivatedRoute,
-		private _sanitizer: DomSanitizer
+		private fb: FormBuilder,
+		private cdr: ChangeDetectorRef
 	) {}
 
 	ngOnInit(): void {
-		this.personaForm = new FormGroup({
-			documento: new FormControl("", [
-				Validators.required,
-				Validators.maxLength(8),
-			]),
-			sexo: new FormControl(null, Validators.required),
-			img: new FormControl(null, Validators.required),
-		});
+		this.initForm();
+	}
 
-		this.form_registro = new FormGroup({
-			nroRegistro: new FormControl("", [Validators.required]),
-			fecha: new FormControl("", [Validators.required]),
-			tipo_registro: new FormControl("", [Validators.required]),
-			motivo_consulta: new FormControl("", [
-				Validators.required,
-				Validators.maxLength(500),
-			]),
-			persona: new FormGroup({
-				nombre: new FormControl("", [Validators.required]),
-				apellido: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(25),
-				]),
-				documento: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(8),
-				]),
-				fechaNacimiento: new FormControl("", [Validators.required]),
-				sexo: new FormControl("", [Validators.required]),
-				telefono1: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(10),
-				]),
-				telefono2: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(10),
-				]),
-				calle: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(20),
-				]),
-				numero: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(4),
-				]),
-				departamento: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(5),
-				]),
-				piso: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(2),
-				]),
-				localidad: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(50),
-				]),
-				cpostal: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(4),
-				]),
-				provincia: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(25),
-				]),
-				pais: new FormControl("", [
-					Validators.required,
-					Validators.maxLength(25),
-				]),
-				img: new FormControl(null, [Validators.required]),
+	initForm(datos?) {
+		this.personaForm = this.fb.group({
+			nroRegistro: [ '', Validators.required ],
+			fecha: [ '', Validators.required ],
+			tipo_registro: [ '', Validators.required ],
+			motivo_consulta: [ '', [ Validators.required, Validators.maxLength(500) ] ],
+			persona: this.fb.group({
+				nombre: [ datos ? datos.nombres : '', Validators.required ],
+				apellido: [ datos ? datos.apellido : '', Validators.required ],
+				documento: [ '', Validators.required ],
+				fechaNacimiento: [ datos ? datos.fechaNacimiento : '', Validators.required ],
+				sexo: [ '', Validators.required ],
+				telefono1: [ '', [ Validators.required, Validators.maxLength(10) ] ],
+				telefono2: [ '', [ Validators.required, Validators.maxLength(10) ] ],
+				calle: [ datos ? datos.calle : '', [ Validators.required, Validators.maxLength(20) ] ],
+				numero: [ datos ? datos.numero : '', [ Validators.required, Validators.maxLength(4) ] ],
+				departamento: [ datos ? datos.departamento : '', [ Validators.required, Validators.maxLength(5) ] ],
+				piso: [ datos ? datos.pais : '', [ Validators.required, Validators.maxLength(2) ] ],
+				localidad: [ datos ? datos.ciudad : '', [ Validators.required, Validators.maxLength(50) ] ],
+				cpostal: [ datos ? datos.cpostal : '', [ Validators.required, Validators.maxLength(4) ] ],
+				provincia: [ datos ? datos.provincia : '', [ Validators.required, Validators.maxLength(25) ] ],
+				pais: [ datos ? datos.pais : '', [ Validators.required, Validators.maxLength(25) ] ],
+				img: [ datos ? datos.foto : '', Validators.required ]
 			}),
-			fecha_ini_sint: new FormControl("", [Validators.required]),
-			sintomas: new FormControl("", [Validators.required]),
-			antencedentes_p: new FormControl("", [Validators.required]),
-			enfermedad_pre: new FormControl("", [Validators.required]),
-			toma_medicamentos: new FormControl("", [Validators.required]),
-			vivienda_personas: new FormControl("", [
-				Validators.required,
-				Validators.maxLength(2),
-			]),
-
-			trabajo: new FormArray([
-				new FormGroup({
-					lugar: new FormControl("", [
-						Validators.required,
-						Validators.maxLength(50),
-					]),
-					telefono: new FormControl("", [
-						Validators.required,
-						Validators.maxLength(10),
-					]),
-					calle: new FormControl("", [
-						Validators.required,
-						Validators.maxLength(25),
-					]),
-					numero: new FormControl("", [
-						Validators.required,
-						Validators.maxLength(4),
-					]),
-					localidad: new FormControl("", [
-						Validators.required,
-						Validators.maxLength(25),
-					]),
-				}),
-			]),
-			usuario: new FormControl("", [Validators.required]),
+			fecha_ini_sint: [ '', Validators.required ],
+			sintomas: [ '', Validators.required ],
+			antencedentes_p: [ '', Validators.required ],
+			enfermedad_pre: [ '', Validators.required ],
+			toma_medicamentos: [ '', Validators.required ],
+			vivienda_personas: [ '', [ Validators.required, Validators.maxLength(2) ] ],
+			trabajo: this.fb.array([ this.buildTrabajo() ]),
+			usuario: new FormControl('', [ Validators.required ])
 		});
 	}
 
-	Renaper() {
-		this.cargando_datos = true;
-		this.buscar_persona = true;
-
-		let params = `documento=${this.personaForm.value.documento}&sexo=${this.personaForm.value.sexo}`;
-		console.log(params);
-		this._renaperService.getPersona(params).subscribe((data: any) => {
-			this.renaper = data;
-			this.cargando_datos = true;
-			this.buscar_persona = true;
-			console.log(this.renaper.datos.foto);
-			this.form_registro.patchValue({
-				persona: { documento: this.personaForm.value.documento },
-			});
-			this.form_registro.patchValue({
-				persona: { sexo: this.personaForm.value.sexo },
-			});
-			this.form_registro.patchValue({
-				persona: { img: this.renaper.datos.foto },
-			});
-			this.form_registro.patchValue({
-				persona: { apellido: this.renaper.datos.apellido },
-			});
-			this.form_registro.patchValue({
-				persona: { nombre: this.renaper.datos.nombres },
-			});
-			this.form_registro.patchValue({
-				persona: { fechaNacimiento: this.renaper.datos.fechaNacimiento },
-			});
-			this.form_registro.patchValue({
-				persona: { calle: this.renaper.datos.calle },
-			});
-			this.form_registro.patchValue({
-				persona: { numero: this.renaper.datos.numero },
-			});
-			this.form_registro.patchValue({
-				persona: { piso: this.renaper.datos.piso },
-			});
-			this.form_registro.patchValue({
-				persona: { departamento: this.renaper.datos.departamento },
-			});
-			this.form_registro.patchValue({
-				persona: { cpostal: this.renaper.datos.cpostal },
-			});
-			this.form_registro.patchValue({
-				persona: { localidad: this.renaper.datos.ciudad },
-			});
-
-			this.form_registro.patchValue({
-				persona: { provincia: this.renaper.datos.provincia },
-			});
-			this.form_registro.patchValue({
-				persona: { pais: this.renaper.datos.pais },
-			});
-			this.router.navigate(["/registro"]);
+	buildTrabajo() {
+		return this.fb.group({
+			lugar: [ '', Validators.required, Validators.maxLength(50) ],
+			telefono: [ '', Validators.required, Validators.maxLength(10) ],
+			calle: [ '', Validators.required, Validators.maxLength(25) ],
+			numero: [ '', Validators.required, Validators.maxLength(4) ],
+			localidad: [ '', Validators.required, Validators.maxLength(25) ]
 		});
+	}
+
+	async renaper() {
+		let params = `documento=${this.personaForm.get('persona.documento').value}&sexo=${this.personaForm.get(
+			'persona.sexo'
+		).value}`;
+
+		this._renaperService.getPersona(params).subscribe((data: any) => {
+			debugger;
+			this.initForm(data.datos);
+			this.cdr.markForCheck();
+		});
+	}
+
+	submit() {
+		debugger;
+		// Acá están todos los datos del formulario para guardar en la BD
+		let datosPersona = this.personaForm.value;
 	}
 }
