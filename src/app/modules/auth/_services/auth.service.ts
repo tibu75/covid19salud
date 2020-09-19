b92738326aa584d84d1dad1fe378d41b27eccc4c
Jose Luis <jls0107@gmail.com>
2020-09-19 01:05:32 -0300

    SessionStorage

--- src/app/modules/auth/_services/auth.service.ts
+++ src/app/modules/auth/_services/auth.service.ts
@@ -16,164 +16,172 @@ import { environment } from "src/environments/environment";
 import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: "root",
+	providedIn: "root",
 })
 export class AuthService implements OnDestroy {
-  // private fields
-  private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
-  private isLoadingSubject: BehaviorSubject<boolean>;
-  private authLocalStorageToken = `${environment.TOKEN}`;
-  private authLocalStorageId = `${environment.ID}`;
-  private authLocalStorageUser = `${environment.USER}`;
+	// private fields
+	private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
+	private isLoadingSubject: BehaviorSubject<boolean>;
+	private authSessionStorageToken = `${environment.TOKEN}`;
+	private authSessionStorageId = `${environment.ID}`;
+	private authSessionStorageUser = `${environment.USER}`;
 
-  // public fields
-  currentUser$: Observable<UserModel>;
-  isLoading$: Observable<boolean>;
-  currentUserSubject: BehaviorSubject<UserModel>;
+	// public fields
+	currentUser$: Observable<UserModel>;
+	isLoading$: Observable<boolean>;
+	currentUserSubject: BehaviorSubject<UserModel>;
 
-  auth2: any;
+	auth2: any;
 
-  get currentUserValue(): UserModel {
-    return this.currentUserSubject.value;
-  }
+	get currentUserValue(): UserModel {
+		return this.currentUserSubject.value;
+	}
 
-  constructor(
-    private ngZone: NgZone,
-    private authHttpService: AuthHTTPService,
-    private router: Router
-  ) {
-    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
-    this.currentUserSubject = new BehaviorSubject<UserModel>(undefined);
-    this.currentUser$ = this.currentUserSubject.asObservable();
-    this.isLoading$ = this.isLoadingSubject.asObservable();
-    const subscr = this.getUserByToken().subscribe();
-    this.unsubscribe.push(subscr);
-  }
+	constructor(
+		private ngZone: NgZone,
+		private authHttpService: AuthHTTPService,
+		private router: Router
+	) {
+		this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+		this.currentUserSubject = new BehaviorSubject<UserModel>(undefined);
+		this.currentUser$ = this.currentUserSubject.asObservable();
+		this.isLoading$ = this.isLoadingSubject.asObservable();
+		const subscr = this.getUserByToken().subscribe();
+		this.unsubscribe.push(subscr);
+	}
 
-  // public methods
-  login(documento: string, password: string): Observable<UserModel> {
-    this.isLoadingSubject.next(true);
+	// public methods
+	login(documento: string, password: string): Observable<UserModel> {
+		this.isLoadingSubject.next(true);
 
-    return this.authHttpService.login(documento, password).pipe(
-      map((auth: any) => {
-        const result = this.setAuthFromLocalStorage(auth);
-        // console.log(auth);
-        return result;
-      }),
-      switchMap(() => this.getUserByToken()),
-      catchError((err) => {
-        console.error("err", err);
-        return of(undefined);
-      }),
-      finalize(() => this.isLoadingSubject.next(false))
-    );
-  }
+		return this.authHttpService.login(documento, password).pipe(
+			map((auth: any) => {
+				const result = this.setAuthFromSessionStorage(auth);
+				// console.log(auth);
+				return result;
+			}),
+			switchMap(() => this.getUserByToken()),
+			catchError((err) => {
+				console.error("err", err);
+				return of(undefined);
+			}),
+			finalize(() => this.isLoadingSubject.next(false))
+		);
+	}
 
-  logout() {
-    localStorage.removeItem(this.authLocalStorageToken);
-    localStorage.removeItem(this.authLocalStorageId);
-    localStorage.removeItem(this.authLocalStorageUser);
-    this.ngZone.run(() => {
-      this.router.navigateByUrl("/auth/login");
-    });
-  }
-  /* this.router.navigate(["/auth/login"], {
+	logout() {
+		sessionStorage.removeItem(this.authSessionStorageToken);
+		sessionStorage.removeItem(this.authSessionStorageId);
+		sessionStorage.removeItem(this.authSessionStorageUser);
+		this.ngZone.run(() => {
+			this.router.navigateByUrl("/auth/login");
+		});
+	}
+	/* this.router.navigate(["/auth/login"], {
       queryParams: {},
     }); */
 
-  getUserByToken(): Observable<UserModel> {
-    const token = this.getAuthFromLocalStorage();
-    const id = this.getIdFromLocalStorage();
+	getUserByToken(): Observable<UserModel> {
+		const token = this.getAuthFromSessionStorage();
+		const id = this.getIdFromSessionStorage();
 
-    if (!token) {
-      return of(undefined);
-    }
+		if (!token) {
+			return of(undefined);
+		}
 
-    this.isLoadingSubject.next(true);
-    // console.log("paso2");
-    return this.authHttpService.getUserByToken(id).pipe(
-      map((user: UserModel) => {
-        // console.log("paso3", user);
-        if (user) {
-          this.currentUserSubject = new BehaviorSubject<UserModel>(user);
-        } else {
-          this.logout();
-        }
-        return user;
-      }),
-      finalize(() => this.isLoadingSubject.next(false))
-    );
-  }
+		this.isLoadingSubject.next(true);
+		// console.log("paso2");
+		return this.authHttpService.getUserByToken(id).pipe(
+			map((user: UserModel) => {
+				// console.log("paso3", user);
+				if (user) {
+					this.currentUserSubject = new BehaviorSubject<UserModel>(user);
+				} else {
+					this.logout();
+				}
+				return user;
+			}),
+			finalize(() => this.isLoadingSubject.next(false))
+		);
+	}
 
-  // need create new user then login
-  registration(user: UserModel): Observable<any> {
-    this.isLoadingSubject.next(true);
-    return this.authHttpService.createUser(user).pipe(
-      map(() => {
-        this.isLoadingSubject.next(false);
-      }),
-      switchMap(() => this.login(user.documento, user.password)),
-      catchError((err) => {
-        console.error("err", err);
-        return of(undefined);
-      }),
-      finalize(() => this.isLoadingSubject.next(false))
-    );
-  }
+	// need create new user then login
+	registration(user: UserModel): Observable<any> {
+		this.isLoadingSubject.next(true);
+		return this.authHttpService.createUser(user).pipe(
+			map(() => {
+				this.isLoadingSubject.next(false);
+			}),
+			switchMap(() => this.login(user.documento, user.password)),
+			catchError((err) => {
+				console.error("err", err);
+				return of(undefined);
+			}),
+			finalize(() => this.isLoadingSubject.next(false))
+		);
+	}
 
-  forgotPassword(documento: string): Observable<boolean> {
-    this.isLoadingSubject.next(true);
-    return this.authHttpService
-      .forgotPassword(documento)
-      .pipe(finalize(() => this.isLoadingSubject.next(false)));
-  }
-  // private methods
-  private setAuthFromLocalStorage(auth: AuthModel): boolean {
-    const { id, token, usuario } = auth;
+	forgotPassword(documento: string): Observable<boolean> {
+		this.isLoadingSubject.next(true);
+		return this.authHttpService
+			.forgotPassword(documento)
+			.pipe(finalize(() => this.isLoadingSubject.next(false)));
+	}
+	// private methods
+	private setAuthFromSessionStorage(auth: AuthModel): boolean {
+		const { id, token, usuario } = auth;
 
-    // store auth token/refreshToken/epiresIn in local storage to keep user logged in between page refreshes
-    if (token) {
-      localStorage.setItem(this.authLocalStorageToken, JSON.stringify(token));
-      localStorage.setItem(this.authLocalStorageId, JSON.stringify(id));
-      localStorage.setItem(this.authLocalStorageUser, JSON.stringify(usuario));
-      return true;
-    }
-    return false;
-  }
+		// store auth token/refreshToken/epiresIn in local storage to keep user logged in between page refreshes
+		if (token) {
+			sessionStorage.setItem(
+				this.authSessionStorageToken,
+				JSON.stringify(token)
+			);
+			sessionStorage.setItem(this.authSessionStorageId, JSON.stringify(id));
+			sessionStorage.setItem(
+				this.authSessionStorageUser,
+				JSON.stringify(usuario)
+			);
+			return true;
+		}
+		return false;
+	}
 
-  private getAuthFromLocalStorage(): AuthModel {
-    try {
-      const authData = JSON.parse(
-        localStorage.getItem(this.authLocalStorageToken)
-      );
-      return authData;
-    } catch (error) {
-      console.error(error);
-      return undefined;
-    }
-  }
-  private getIdFromLocalStorage(): AuthModel {
-    try {
-      const authId = JSON.parse(localStorage.getItem(this.authLocalStorageId));
-      return authId;
-    } catch (error) {
-      console.error(error);
-      return undefined;
-    }
-  }
-  private getUserFromLocalStorage(): AuthModel {
-    try {
-      const authUser = JSON.parse(
-        localStorage.getItem(this.authLocalStorageUser)
-      );
-      return authUser;
-    } catch (error) {
-      console.error(error);
-      return undefined;
-    }
-  }
+	private getAuthFromSessionStorage(): AuthModel {
+		try {
+			const authData = JSON.parse(
+				sessionStorage.getItem(this.authSessionStorageToken)
+			);
+			return authData;
+		} catch (error) {
+			console.error(error);
+			return undefined;
+		}
+	}
+	private getIdFromSessionStorage(): AuthModel {
+		try {
+			const authId = JSON.parse(
+				sessionStorage.getItem(this.authSessionStorageId)
+			);
+			return authId;
+		} catch (error) {
+			console.error(error);
+			return undefined;
+		}
+	}
+	private getUserFromSessionStorage(): AuthModel {
+		try {
+			const authUser = JSON.parse(
+				sessionStorage.getItem(this.authSessionStorageUser)
+			);
+			return authUser;
+		} catch (error) {
+			console.error(error);
+			return undefined;
+		}
+	}
 
-  ngOnDestroy() {
-    this.unsubscribe.forEach((sb) => sb.unsubscribe());
-  }
+	ngOnDestroy() {
+		this.unsubscribe.forEach((sb) => sb.unsubscribe());
+	}
 }
