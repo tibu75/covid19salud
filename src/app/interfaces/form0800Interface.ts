16e1952061f865f730a5ff3d185d26b688243e9c
Jose Luis <jls0107@gmail.com>
2020-09-19 22:52:06 -0300

    cambios mod y inter

--- src/app/interfaces/form0800Interface.ts
+++ src/app/interfaces/form0800Interface.ts
@@ -1,37 +1,45 @@
 export interface Form0800 {
-	nroRegistro: string;
-	fecha: string;
-	tipo_registro: string;
-	motivo_consulta: string;
-	persona: {
-		nombre: string;
-		apellido: string;
-		documento: string;
-		fechaNacimiento: string;
-		sexo: string;
-		telefono: string;
-		calle: string;
-		numero: string;
-		departamento: string;
-		piso: string;
-		localidad: string;
-		img: string;
-	};
-	fecha_ini_sint: string;
-	sintomas: string;
-	antencedentes_p: string;
-	enfermedad_pre: string;
-	toma_medicamentos: string;
-	vivienda_personas: string;
-
-	trabajo: [
-		{
-			telefono: string;
-			calle: string;
-			numero: string;
-			localidad: string;
-		}
-	];
-
-	usuario: string;
+  nroForm: string;
+  fecha: string;
+  tipo_registro: string;
+  motivo_consulta: string;
+  atencion_domiciliaria: string;
+  cert_aislamiento: string;
+  resultado_hisopado: string;
+  derivacion_107: string;
+  mov_propia: string;
+  criterio_hisopado: string;
+  realizo_hisopado: string;
+  fecha_ini_sint: string;
+  sintomas: string;
+  antencedentes_p: string;
+  enfermedad_pre: string;
+  toma_medicamentos: string;
+  vivienda_personas: string;
+  lugar_hisopado: string;
+  fecha_hisopado: string;
+  persona: {
+    nombre: string;
+    apellido: string;
+    documento: string;
+    fechaNacimiento: string;
+    sexo: string;
+    telefono: string;
+    calle: string;
+    numero: string;
+    departamento: string;
+    piso: string;
+    localidad: string;
+    img: string;
+  };
+  trabajo: [
+    {
+      telefono: string;
+      calle: string;
+      numero: string;
+      localidad: string;
+    }
+  ];
+  cierre_contacto: string;
+  usuario: string;
 }
